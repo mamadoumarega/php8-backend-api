984ed25d8bf6d260a0e8f32621c173e53c426772
Mamadou MARÉGA <hamshakour93@gmail.com>
2024-05-04 02:15:54 +0200

    Adding index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+    namespace Mamadou\Php8BackendApi;
+
+   require __DIR__ . '/vendor/autoload.php';
+   require __DIR__ . '/src/routes/routes.php';
+   require __DIR__ . '/src/helpers/headers.inc.php';
