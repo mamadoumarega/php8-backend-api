0a25d81746fadc3f0df88b311f8468e0448b3511
Mamadou MARÉGA <hamshakour93@gmail.com>
2024-05-05 22:27:11 +0200

    Add whoops Json Response Handler for API response

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
-    namespace Mamadou\Php8BackendApi;
+   namespace Mamadou\Php8BackendApi;
+
+   use Whoops\Handler\JsonResponseHandler;
+   use Whoops\Run;
 
    require __DIR__ . '/vendor/autoload.php';
+
+   $whoops = new Run();
+   $whoops->pushHandler(new JsonResponseHandler);
+   $whoops->register();
+   
    require __DIR__ . '/src/routes/routes.php';
    require __DIR__ . '/src/config/config.inc.php';
    require __DIR__ . '/src/config/database.inc.php';
